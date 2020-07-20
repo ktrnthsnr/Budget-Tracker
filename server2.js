7c220d4b5a8c9b9c38f9a8361544a8a501dfa475
ktrnthsnr <asakthisner@hotmail.com>
2020-07-20 00:03:27 -0700

    reverted server updates

--- server2.js
+++ server2.js
@@ -9,20 +9,33 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
-
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/htmlRoutes.js"));
+// require('./routes/apiRoutes')(app);
+// require('./routes/api')(app);
+// require('./routes/htmlRoutes')(app);
+
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/imageperformance',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+)
+
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
